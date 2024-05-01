be399947cd470506074fe1c8db0bdfdee871c882
vinaygrp1 <vinaygrp1@gmail.com>
2024-05-01 12:32:03 +0530

    expense-shell-1

--- backend.sh
+++ backend.sh
@@ -1,35 +1,10 @@
 #!/bin/bash
-USERID=$(id -u)
-TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
+source ./common.sh
+
+check_root 
 
 echo "Please enter DB password:"
 read -s mysql_root_password
-
-
-VALIDATE(){
-   if [ $1 -ne 0 ]
-   then
-        echo -e "$2...$R FAILURE $N"
-        exit 1
-    else
-        echo -e "$2...$G SUCCESS $N"
-    fi
-}
-
-if [ $USERID -ne 0 ]
-
-then
-    echo "please run this script with root access."
-    exit 1 #manually exit if error comes
-else
-    echo "you are super user."
-fi
 
 dnf module disable nodejs -y &>>$LOGFILE
 VALIDATE $? "Disabling default nodejs"
--- frontend.sh
+++ frontend.sh
@@ -1,31 +1,7 @@
 #!/bin/bash
-USERID=$(id -u)
-TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
+source ./common.sh
 
-VALIDATE(){
-   if [ $1 -ne 0 ]
-   then
-        echo -e "$2...$R FAILURE $N"
-        exit 1
-    else
-        echo -e "$2...$G SUCCESS $N"
-    fi
-}
-
-if [ $USERID -ne 0 ]
-
-then
-    echo "please run this script with root access."
-    exit 1 #manually exit if error comes
-else
-    echo "you are super user."
-fi
+check_root 
 
 dnf install nginx -y &>>$LOGFILE
 VALIDATE $? "Installing nginx"
