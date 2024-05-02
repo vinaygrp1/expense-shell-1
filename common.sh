e91daebbbae24167cd38a4366127994b3d2aad75
vinaygrp1 <vinaygrp1@gmail.com>
2024-05-02 13:07:42 +0530

    expense-shell-1

--- common.sh
+++ common.sh
@@ -28,7 +28,7 @@ check_root(){
     if [ $USERID -ne 0 ]
     then
         echo "please run this script with root access."
-        exit 1 #manually exit if error comes.
+        exit 1 # manually exit if error comes.
     else
         echo "you are super user."
     fi
