f30e1048a91edca5ac78b1d458462c34e42f6f2e
vinaygrp1 <vinaygrp1@gmail.com>
2024-05-02 15:29:24 +0530

    expense-shell-1/

--- backend.sh
+++ backend.sh
@@ -7,58 +7,51 @@ echo "Please enter DB password:"
 read -s mysql_root_password
 
 dnf module disable nodejs -y &>>$LOGFILE
-VALIDATE $? "Disabling default nodejs"
+
 
 dnf module enable nodejs:20 -y &>>$LOGFILE
-VALIDATE $? "Enabling nodejs:20 version"
+
 
 dnf install nodejs -y &>>$LOGFILE
-VALIDATE $? "Installing nodejs"
+
 
 id expense &>>$LOGFILE
 
 if [ $? -ne 0 ]
 then 
     useradd expense &>>$LOGFILE
-    VALIDATE $? "Creating expense user"
+    
 else #if [ $? eq 0]
     echo -e "Expense user already created...$Y SKIPPING $N"
 fi
 
 mkdir -p /app &>>$LOGFILE
-VALIDATE $? "Creating app directory"
 
 curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOGFILE
-VALIDATE $? "Downloading backend code"
 
 cd /app
 rm -rf /app/*
 
 unzip /tmp/backend.zip &>>$LOGFILE
-VALIDATE $? "Extracted backend code"
 
 npm install &>>$LOGFILE
-VALIDATE $? "Installing nodejs dependencies"
-
+#check your repo and path
 cp /home/ec2-user/expense-shell/backend.service /etc/systemd/system/backend.service &>>$LOGFILE
 
 systemctl daemon-reload &>>$LOGFILE
-VALIDATE $? "Daemon-reload"
 
 systemctl start backend &>>$LOGFILE
-VALIDATE $? "Starting backend"
 
 systemctl enable backend &>>$LOGFILE
-VALIDATE $? "Enabling backend"
+
 
 dnf install mysql -y &>>$LOGFILE
-VALIDATE $? "Instaling MySQL client"
+
 
 mysql -h database.vinaydevops.online -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>$LOGFILE
-VALIDATE $? "Schema Loading"
 
 systemctl restart backend &>>$LOGFILE
-VALIDATE $? "Restart backend"
+
 
 
 
